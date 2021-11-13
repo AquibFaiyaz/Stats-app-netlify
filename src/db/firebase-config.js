9552b7e74d7ac3cbca190f85a84a61c9cedda809
Aquib Faiyaz <faiyazaquib28@gmail.com>
2021-11-13 21:57:14 +0530

    config

--- src/db/firebase-config.js
+++ src/db/firebase-config.js
@@ -12,13 +12,13 @@ const {
 } = process.env;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPOy28MRP0a3-u7kMV_hT1TYT7bql4P00",
-  authDomain: "statistics-app-c8002.firebaseapp.com",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
   projectId: "statistics-app-c8002",
   storageBucket: "statistics-app-c8002.appspot.com",
-  messagingSenderId: "664193077056",
-  appId: "1:664193077056:web:5d6838da428deae2e1b4a5",
-  measurementId: "G-N7SQYPFM61",
+  messagingSenderId: REACT_APP_MESSEGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
