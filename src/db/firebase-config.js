c6fdc459949f34eae1073f0b7c1d464992e002ff
Aquib Faiyaz <faiyazaquib28@gmail.com>
2021-11-13 23:34:04 +0530

    changes 3

--- src/db/firebase-config.js
+++ src/db/firebase-config.js
@@ -15,7 +15,8 @@ const firebaseConfig = {
   apiKey: REACT_APP_API_KEY,
   authDomain: REACT_APP_AUTH_DOMAIN,
   projectId: "statistics-app-c8002",
-  storageBucket: "statistics-app-c8002.appspot.com",
+
+  storageBucket: REACT_APP_STORAGE_BUCKET,
   messagingSenderId: REACT_APP_MESSEGING_SENDER_ID,
   appId: REACT_APP_APP_ID,
   measurementId: REACT_APP_MEASUREMENT_ID,
